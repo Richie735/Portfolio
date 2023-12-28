301d849dfff1d5764167009cf14fc3b3d61e5c98
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-12-28 16:48:20 +0000

    :bug: Fix: wrong code on hud

--- components/hud.tsx
+++ components/hud.tsx
@@ -1,47 +1,6 @@
-'use client';
-
-import { useEffect, useState } from 'react';
 import { LuGithub, LuLinkedin } from 'react-icons/lu';
 
 function Hud() {
-  const [text, setText] = useState('Your Text Here');
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-  useEffect(() => {
-    const handleMouseOver = () => {
-      let iteration = 0;
-      const interval = setInterval(() => {
-        setText((prevText) =>
-          prevText
-            .split('')
-            .map((letter, index) => {
-              if (index < iteration) {
-                return letter;
-              }
-              return letters[Math.floor(Math.random() * 26)];
-            })
-            .join(''),
-        );
-        if (iteration >= text.length) {
-          clearInterval(interval);
-        }
-        iteration += 1 / 3;
-      }, 30);
-    };
-
-    const h1Element = document.querySelector('h1');
-    if (h1Element) {
-      h1Element.addEventListener('mouseover', handleMouseOver);
-
-      // Clean up function
-      return () => {
-        h1Element.removeEventListener('mouseover', handleMouseOver);
-      };
-    }
-    // Return a no-op function if h1Element is null
-    return () => {};
-  }, [text, letters]);
-
   return (
     <div className="relative z-50">
       <div className="fixed bottom-0 left-4containers flex flex-col items-center">
