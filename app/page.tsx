c0145617aecdf6873fc2ff8f32a24f3c08358694
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-12-28 00:56:02 +0000

    :sparkles: Feat: Update Home component
    Updated the Home component inserting Hud and Hero components.
    Completing Hud visual effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Hero from '@/components/hero';
+import Hud from '@/components/hud';
+
 export default function Home() {
-  return <main className="" />;
+  return (
+    <main className="relative">
+      <Hud />
+      <div className="secondary-gradient absolute left-[342px] z-0 mt-[60px] h-full w-px" />
+      <Hero />
+    </main>
+  );
 }
