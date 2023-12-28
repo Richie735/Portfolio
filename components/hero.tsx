2031893daa9a0273079924674812189b4561d343
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-12-28 00:49:45 +0000

    :sparkles: Feat: Create Hero component
    Created the Hero component

---
+++ components/hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <section className="mt-[380px] h-screen px-4containersExtraGap">
+      <p className="ml-[100px] font-spaceMono text-base font-normal tracking-widest">
+        Hi, I&apos;m
+      </p>
+      <h1 className=" ml-1container1gap line-clamp-none font-spaceMono text-7xl font-bold tracking-tighter">
+        RICARDO PEREIRA
+      </h1>
+      <p className="ml-[100px] font-spaceMono text-xl font-normal  tracking-wider   text-primary">
+        Software Engineer{' '}
+      </p>
+    </section>
+  );
+}
