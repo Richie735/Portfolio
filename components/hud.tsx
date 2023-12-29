cf5dc256d0333c7b4463bf2d129c57542107e860
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-12-29 18:59:15 +0000

    :sparkles: Feat: Added hover effect to icons
    Enhanced user experience by adding a hover effect to icons along with tooltips that display the icon name. This improvement increases user understanding of each icon's functionality.
    
    Additionally, included the navbar component in the Hud component for improved navigation and a cohesive user interface.

--- components/hud.tsx
+++ components/hud.tsx
@@ -1,26 +1,47 @@
+'use client';
+
 import Link from 'next/link';
 import { LuGithub, LuLinkedin } from 'react-icons/lu';
+import Navbar from './navbar';
 
 function Hud() {
   return (
-    <div className="relative z-50">
-      <div className="fixed bottom-0 left-4containers flex flex-col items-center">
-        <div className="h-3 w-3 rounded-full bg-primary" />
-        <div className="primary-gradient h-[50vh] w-px" />
+    <>
+      <Navbar />
+      <div className="relative z-50">
+        <div className="fixed bottom-0 left-4containers flex flex-col items-center">
+          <div className="h-3 w-3 rounded-full bg-primary" />
+          <div className="primary-gradient h-[50vh] w-px" />
+        </div>
+        <div className="fixed right-4containers top-0 flex flex-col items-center">
+          <div className=" primary-gradient h-[90px] w-px rotate-180" />
+
+          <div className="group flex items-center">
+            <div className="border-px absolute z-40 h-fit w-fit translate-x-10 translate-y-2  rounded-md border border-primary border-opacity-10 bg-[#222222] px-2 py-0.5 opacity-0 group-hover:opacity-100">
+              <p className=" font-spaceMono text-xs font-light text-primary">
+                Linkedin
+              </p>
+            </div>
+            <Link
+              href="https://www.linkedin.com/in/ricardopereira735/"
+              target="_blank"
+            >
+              <LuLinkedin className="hover:scale mt-4 h-6 w-6 text-primary  hover:text-white" />
+            </Link>
+          </div>
+          <div className="group">
+            <div className="border-px absolute z-40 h-fit w-fit translate-x-10 translate-y-4 rounded-md border  border-primary border-opacity-10 bg-[#222222] px-2 py-0.5 opacity-0 group-hover:opacity-100 ">
+              <p className=" font-spaceMono text-xs font-light text-primary">
+                Github
+              </p>
+            </div>
+            <Link href="https://github.com/Richie735" target="_blank">
+              <LuGithub className=" mt-4 h-6 w-6 text-primary  hover:scale-110 hover:text-white" />
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="fixed right-4containers top-0 flex flex-col items-center">
-        <div className="primary-gradient h-[90px] w-px rotate-180" />
-        <Link
-          href="https://www.linkedin.com/in/ricardopereira735/"
-          target="_blank"
-        >
-          <LuLinkedin className=" hover:scale mt-4 h-6 w-6 text-primary hover:-translate-y-1 hover:text-white" />
-        </Link>
-        <Link href="https://github.com/Richie735" target="_blank">
-          <LuGithub className=" mt-4 h-6 w-6 text-primary hover:-translate-y-1 hover:scale-110 hover:text-white" />
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
 
