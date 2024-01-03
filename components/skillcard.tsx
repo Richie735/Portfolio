938937d75073cf7bae8b8cb136899becc7280148
Richie735 <93219607+Richie735@users.noreply.github.com>
2024-01-03 15:46:12 +0000

    :sparkles: Feat: Created SkillCard component

---
+++ components/skillcard.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+type SkillCardProps = {
+  title: string;
+  icon: string;
+};
+const SkillCard = ({ title, icon }: SkillCardProps) => {
+  return (
+    <div className="about__skill-card relative h-1container w-1container rounded-xl bg-background bg-opacity-20">
+      <div className="about__skill-card--border"></div>
+      <div className="relative z-[2] m-px h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-xl bg-secondary">
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <div
+            dangerouslySetInnerHTML={{ __html: icon }}
+            className="h-7 w-7"
+          ></div>
+          <p className="mt-1 font-spaceMono text-xs font-normal tracking-widest text-text-primary">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkillCard;
