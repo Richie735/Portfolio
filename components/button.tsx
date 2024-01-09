5fd39ead51bccf686317a076cd601ed35ab3d5a7
Richie735 <93219607+Richie735@users.noreply.github.com>
2024-01-09 18:41:30 +0000

    :bug: Fix: Button hover props not working

--- components/button.tsx
+++ components/button.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 type ButtonProps = {
   title: string;
   link?: string | null;
+  textSize?: string;
   bgColor?: string;
   textColor?: string;
   hoverBgColor?: string;
@@ -14,21 +15,29 @@ type ButtonProps = {
 const Button = ({
   title,
   link,
+  textSize,
   bgColor,
   textColor,
   hoverBgColor,
   hoverTextColor,
 }: ButtonProps) => {
+  const hBgColor = hoverBgColor
+    ? 'hover:bg-' + hoverBgColor
+    : 'hover:bg-primary';
+  const hTxColor = hoverTextColor
+    ? 'hover:text-' + hoverTextColor
+    : 'hover:text-primary';
+
   return (
     <Link
       type="button"
       href={link ? link : '#'}
-      className={`button__clip-path relative flex h-10 cursor-pointer items-center  justify-center overflow-hidden rounded px-5 font-spaceMono text-xl font-bold tracking-wide 
-      ${textColor ? 'text-' + textColor : 'text-background'}  ${
-        bgColor ? 'bg-' + bgColor : 'bg-primaryFade'
-      } ${hoverBgColor ? 'hover:' + hoverBgColor : 'hover:bg-primary'} ${
-        hoverTextColor ? 'hover:' + hoverBgColor : ''
-      } `}
+      className={`button__clip-path relative flex h-10 cursor-pointer items-center  justify-center overflow-hidden rounded px-5 font-spaceMono font-bold tracking-wide transition-colors duration-200 ease-in-out
+    ${textSize ? 'text-' + textSize : 'text-xl'}
+    ${bgColor ? 'bg-' + bgColor : 'bg-primaryFade'} 
+    ${textColor ? 'text-' + textColor : 'text-background'}  
+    ${hBgColor}
+    ${hTxColor}`}
     >
       {title}
     </Link>
